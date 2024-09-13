0e7c522310c835e7c34764ba7a2ba38871dc4ab5
KarreKult <kareemzuwa@gmail.com>
2024-09-14 01:07:15 +0200

    structuring component directory

---
+++ src/app/components/board/board.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-board',
+  standalone: true,
+  templateUrl: './board.component.html',
+  styleUrl: './board.component.css',
+})
+export class AppBoard {}
---
+++ src/app/components/player/player.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-player',
+  standalone: true,
+  templateUrl: './player.component.html',
+  styleUrl: './player.component.css',
+})
+export class AppPlayer {}
