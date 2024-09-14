abc0d35028b5c92571446c0db2df29495e2c90fe
KarreKult <kareemzuwa@gmail.com>
2024-09-14 11:02:58 +0200

    fixing main background and static board table

---
+++ src/app/components/close-icon/close-icon.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-close-icon',
+  standalone: true,
+  templateUrl: './close-icon.component.html',
+  styleUrl: './close-icon.component.css',
+})
+export class AppCloseIcon {}
