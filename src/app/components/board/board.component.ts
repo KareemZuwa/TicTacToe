404ea1c88da9bd26c94c9ce3cae36ddaf9f52fdf
KarreKult <kareemzuwa@gmail.com>
2024-09-14 14:39:17 +0200

    adding cell clicking logic

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -1,9 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-board',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './board.component.html',
   styleUrl: './board.component.css',
 })
-export class AppBoard {}
+export class AppBoard {
+  board: string[][] = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', ''],
+  ];
+
+  currentPlayer: string = 'X';
+
+  handleClick(row: number, col: number): void {
+    if (this.board[row][col] === '') {
+      this.board[row][col] = this.currentPlayer;
+      console.log(
+        `Player ${this.currentPlayer} clicked on cell [${row}, ${col}]`
+      );
+
+      this.currentPlayer = this.currentPlayer === 'X' ? 'O' : 'X';
+    }
+    console.table(this.board);
+  }
+}
