5c20f6def5a119e645b18414f2a96b15ea67bb80
KarreKult <kareemzuwa@gmail.com>
2024-09-14 22:09:45 +0200

    styling changes to imporve experience

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -16,18 +16,93 @@ export class AppBoard {
     ['', '', ''],
   ];
 
-  currentPlayer: string = 'X';
+  currentPlayer: string = 'O';
+  winner: string | null = null;
+  isDraw: boolean = false;
 
   handleClick(row: number, col: number): void {
-    if (this.board[row][col] === '') {
+    if (this.board[row][col] === '' && !this.winner) {
       this.board[row][col] = this.currentPlayer;
       console.log(
         `Player ${this.currentPlayer} clicked on cell [${row}, ${col}]`
       );
 
-      this.currentPlayer = this.currentPlayer === 'X' ? 'O' : 'X';
+      if (this.checkWinner()) {
+        console.log(`Player ${this.winner} wins!`);
+      } else if (this.checkDraw()) {
+        this.isDraw = true;
+        console.log('The game is a draw!');
+      } else {
+        this.currentPlayer = this.currentPlayer === 'X' ? 'O' : 'X';
+      }
     }
     console.table(this.board);
+  }
+
+  checkWinner(): boolean {
+    const winningCombinations = [
+      // Rows
+      [
+        [0, 0],
+        [0, 1],
+        [0, 2],
+      ],
+      [
+        [1, 0],
+        [1, 1],
+        [1, 2],
+      ],
+      [
+        [2, 0],
+        [2, 1],
+        [2, 2],
+      ],
+      // Columns
+      [
+        [0, 0],
+        [1, 0],
+        [2, 0],
+      ],
+      [
+        [0, 1],
+        [1, 1],
+        [2, 1],
+      ],
+      [
+        [0, 2],
+        [1, 2],
+        [2, 2],
+      ],
+      // Diagonals
+      [
+        [0, 0],
+        [1, 1],
+        [2, 2],
+      ],
+      [
+        [0, 2],
+        [1, 1],
+        [2, 0],
+      ],
+    ];
+
+    for (const combo of winningCombinations) {
+      const [a, b, c] = combo;
+      if (
+        this.board[a[0]][a[1]] !== '' &&
+        this.board[a[0]][a[1]] === this.board[b[0]][b[1]] &&
+        this.board[a[0]][a[1]] === this.board[c[0]][c[1]]
+      ) {
+        this.winner = this.board[a[0]][a[1]];
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  checkDraw(): boolean {
+    return this.board.every((row) => row.every((cell) => cell !== ''));
   }
 
   resetBoard(): void {
--- src/app/components/player/player.component.ts
+++ src/app/components/player/player.component.ts
@@ -1,13 +1,18 @@
 import { Component, Input } from '@angular/core';
 import { AppCircleIcon } from '../circle-icon/circle-icon.component';
+import { AppCloseIcon } from '../close-icon/close-icon.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-player',
   standalone: true,
-  imports: [AppCircleIcon],
+  imports: [AppCircleIcon, AppCloseIcon, CommonModule],
   templateUrl: './player.component.html',
   styleUrl: './player.component.css',
 })
 export class AppPlayer {
-  @Input() player = '';
+  @Input() player: string = '';
+  @Input() backgroundColor: string = '#ECAF4F';
+  @Input() icon: string = 'circle';
+  @Input() isCurrentPlayer: boolean = false;
 }
